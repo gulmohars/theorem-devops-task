6cc6a9ea74614fd066efe092cd5e924f204cf82a
Alberto Eduardo <elpasta@gmail.com>
2019-11-21 11:32:42 +0100

    go application exposing HTTP API

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # theorem-devops-task
 Theorem LLC - DevOps Prototyping Exercise
+
+Go Application.
+
+I used golang to create a simple application exposing an HTTP API with 2 endpoints:
+  -  (/) Endpoint 1: Json payload response
+  -  (/health) Endpoint 2: used for healthcheck
+
+The application will run on port 8080
---
+++ src/main.go
@@ -0,0 +1,35 @@
+
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Devops struct {
+  Name    string
+  Hobbies []string
+}
+
+func getDevops(w http.ResponseWriter, r *http.Request) {
+	answer := Devops{"Alberto", []string{"AWS", "Kubernetes"}}
+
+	js, err := json.Marshal(answer)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+    	return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	http.HandleFunc("/", getDevops)
+	http.HandleFunc("/health", health)
+	http.ListenAndServe(":8080", nil)
+}
